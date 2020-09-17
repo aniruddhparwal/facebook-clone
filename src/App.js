d5b684c715f50a6ce6a8fbb860e057ab3b12eb53
aniruddhparwal <aniruddhparwal@gmail.com>
2020-09-17 23:56:39 +0530

    Scss added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './css/style.css'
 
 function App() {
   return (
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDCQTPvuYRX-5WDHU6-JymM4XuIpEO8UTM",
+    authDomain: "facebook-clone-ani.firebaseapp.com",
+    databaseURL: "https://facebook-clone-ani.firebaseio.com",
+    projectId: "facebook-clone-ani",
+    storageBucket: "facebook-clone-ani.appspot.com",
+    messagingSenderId: "1066642581601",
+    appId: "1:1066642581601:web:f15ef0182e838ed658e220"
+};
