79c4f63cf31c1f712bedcf1157dedc78d8005bf1
aniruddhparwal <aniruddhparwal@gmail.com>
2020-09-21 23:22:31 +0530

    test

--- src/components/Story.js
+++ src/components/Story.js
@@ -4,6 +4,7 @@ import React from 'react'
 function Story({ image, profileSrc, title }) {
     return (
         <div className="story">
+            <img src={image} />
             <Avatar src={profileSrc} />
             <h4>{title}</h4>
         </div>
