86b383ee6ea3aeabe1a96b4872e86e9512174612
aniruddhparwal <aniruddhparwal@gmail.com>
2020-09-24 01:41:25 +0530

    Updates

---
+++ precache-manifest.b65ce5a082eb1a6bde18511ca2c2499f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913b479a13bae331f0b0a76b849e6983",
+    "url": "/facebook-clone/index.html"
+  },
+  {
+    "revision": "ff67f73e1be96733550a",
+    "url": "/facebook-clone/static/css/main.dc4c2be0.chunk.css"
+  },
+  {
+    "revision": "7ffee3f5e04ebe13326b",
+    "url": "/facebook-clone/static/js/2.b2f6666a.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/facebook-clone/static/js/2.b2f6666a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff67f73e1be96733550a",
+    "url": "/facebook-clone/static/js/main.b26b57d6.chunk.js"
+  },
+  {
+    "revision": "8d9db31eeef904c1dc55",
+    "url": "/facebook-clone/static/js/runtime-main.389e76a3.js"
+  }
+]);
